ab3ad02b4042015d20f7c770208593ff906766ff
emir <emir.beau@mail.ru>
2023-05-16 15:32:59 +0600

    imagefield

--- tv_start/sport_matches/models.py
+++ tv_start/sport_matches/models.py
@@ -14,7 +14,7 @@ class SportType(models.Model):
 class Team(models.Model):
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100, blank=True)
-    logo = models.URLField(blank=True)
+    logo = models.ImageField(blank=True)
     def __str__(self):
         return self.name
 
