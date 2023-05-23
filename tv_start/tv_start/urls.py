2c024720e10b8b503a98c8e8194a3c789756e5f6
emir <emir.beau@mail.ru>
2023-05-23 14:28:16 +0600

    test

--- tv_start/tv_start/urls.py
+++ tv_start/tv_start/urls.py
@@ -26,7 +26,7 @@ from title.views import *
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
-from . import views
+# from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
